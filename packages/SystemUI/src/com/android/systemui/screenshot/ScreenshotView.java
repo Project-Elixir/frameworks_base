fbc8124c984f7f1f67b0fe49aa19ca224f703702
Timi Rautamäki <timi.rautamaki@gmail.com>
2022-04-22 21:01:37 +0000

    SystemUI: screenshot: open the screenshot instead of edit
    Open the taken screenshot in gallery application when clicking on the
    preview. There is a seperate edit button, so the preview imageview
    should open gallery instead.
    @nishant6342: Adapt for android13 qpr2
    
    Test: atest com.android.systemui.screenshot.ScreenshotNotificationSmartActionsTest
    Change-Id: I8f882ddd0da47ca50acc37d25ee0866ce5698e4f
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
@@ -827,7 +827,7 @@ public class ScreenshotView extends FrameLayout implements
                         imageData.owner.getIdentifier(), true);
             } else {
                 startSharedTransition(
-                        imageData.editTransition.get());
+                        imageData.viewTransition.get());
             }
         });
         if (mQuickShareChip != null) {

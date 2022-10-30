2a65fb12cd7ae3adf5b07a5111530d89f182446b
Asriadi Rahim <asriadirahim03@gmail.com>
2022-10-30 23:36:59 +0800

    core: comment public static build consistent
    fix showing error when device started

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1249,7 +1249,7 @@ public class Build {
      */
     public static boolean isBuildConsistent() {
         // Don't care on eng builds.  Incremental build may trigger false negative.
-        if (IS_ENG) return true;
+        /*if (IS_ENG) return true;
 
         if (IS_TREBLE_ENABLED) {
             // If we can run this code, the device should already pass AVB.

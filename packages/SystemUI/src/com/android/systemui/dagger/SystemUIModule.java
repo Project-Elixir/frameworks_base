fee4b37931c03a0333ee9ce4054fdba3f7021e0e
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2023-07-07 08:03:58 +0000

    base: Init Elixir QS Module
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -54,6 +54,7 @@ import com.android.systemui.display.DisplayModule;
 import com.android.systemui.doze.dagger.DozeComponent;
 import com.android.systemui.dreams.dagger.DreamModule;
 import com.android.systemui.dump.DumpManager;
+import com.android.systemui.elixir.ElixirQSModule;
 import com.android.systemui.flags.FeatureFlags;
 import com.android.systemui.flags.FlagsModule;
 import com.android.systemui.keyboard.KeyboardModule;
@@ -188,6 +189,7 @@ import javax.inject.Named;
             ControlsModule.class,
             DemoModeModule.class,
             DisableFlagsModule.class,
+            ElixirQSModule .class,
             FalsingModule.class,
             FlagsModule.class,
             SystemPropertiesFlagsModule.class,

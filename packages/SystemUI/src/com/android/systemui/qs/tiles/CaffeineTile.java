fea378b8ff2b238e9f34d2a228c84c2e71c2ea7e
Mayur <ultramayur123@gmail.com>
2023-11-16 14:10:05 +0530

    fixup! SystemUI: Add caffeine QS tile
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -42,6 +42,7 @@ import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.qs.QSTile.BooleanState;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.qs.QSHost;
+import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tileimpl.QSTileImpl;
 
@@ -71,6 +72,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
     @Inject
     public CaffeineTile(
             QSHost host,
+            QsEventLogger uiEventLogger,
             @Background Looper backgroundLooper,
             @Main Handler mainHandler,
             FalsingManager falsingManager,
@@ -79,7 +81,7 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
             ActivityStarter activityStarter,
             QSLogger qsLogger
     ) {
-        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+        super(host, uiEventLogger, backgroundLooper, mainHandler, falsingManager, metricsLogger,
                 statusBarStateController, activityStarter, qsLogger);
         mWakeLock = mContext.getSystemService(PowerManager.class).newWakeLock(
                 PowerManager.FULL_WAKE_LOCK, "CaffeineTile");
